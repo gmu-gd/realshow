54d94ff03a2080acbb2014d04b92812b6359bec1
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2022-11-28 20:59:15 -0500

    update production homepage

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // run function on initial page load
+    collage();
+    imgSize();
+    // run function on resize of the window
+    $(window).resize(function () {
+
+    });
+    // run function on scroll
+    $(window).scroll(function () {
+
+    });
+    // $(window).load(function() {
+    //   loader();
+    // });
+});
+function imgSize() {
+  var ids = ["#hat","#bow", "#camera", "#numbers", "#pipe", "#smoke", "#stand", "#viewfinder"];
+  var w = $("#container").width();
+  var h = $("#container").height();
+  for (let i = 0; i < ids.length; i++) {
+    var idw = $(ids[i]).width();
+    var nw = idw * .3;
+    $(ids[i]).css("width", nw);
+    var nh = $(ids[i]).height();
+    var xpos = Math.floor(Math.random() * (w - nw));
+    var ypos = Math.floor(Math.random() * (h - nh - 200));
+    $(ids[i]).css("top", (ypos + 25));
+    $(ids[i]).css("left", (xpos + 100));
+    $(ids[i]).removeClass("loading");
+  }
+}
+function collage() {
+  $("#hat").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#bow").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#camera").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#numbers").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#pipe").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#smoke").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#stand").draggable({ containment: "#container", scroll: false, stack: "img" });
+  $("#viewfinder").draggable({ containment: "#container", scroll: false, stack: "img" });
+}
